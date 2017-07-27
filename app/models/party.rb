cc131855a9d10fbc70ca6b29877f9771f23a79bf
罗泷 <godluos@luolongdeMacBook-Air.local>
2017-07-27 16:58:02 +0800

    add post fuction

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,5 @@
 class Party < ApplicationRecord
   belongs_to :user
+  has_many :post
   validates :title, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :parties
+  resources :parties do
+    resources :post
+  end
   devise_for :users
   root "parties#index"
 end
